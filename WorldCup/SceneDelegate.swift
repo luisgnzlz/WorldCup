a5c8f18f4ff6a65fa92cdf8194ceb38279922959
Luis <luisgnzlz33@gmail.com>
2021-08-31 13:45:04 -0700

    updated the delegate

--- WorldCup/SceneDelegate.swift
+++ WorldCup/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = BottomMenuViewController()
+        window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
     }
 
